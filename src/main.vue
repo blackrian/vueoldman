e8c0e7c38d01e62008af3f533db2f34193737f15
Administrator <menghuiw>
2016-08-25 11:54:03 +0800

    结构新加

--- src/main.vue
+++ src/main.vue
@@ -2,13 +2,15 @@
     <div class="page">
         <h1>这个一个首页</h1>
         <p>{{msg}}</p>
+
     </div>
 </template>
 <script>
     export default{
         data(){
             return{
-                msg:'首页测试'
+                msg:'首页测试',
+                sdfasd:"fajsdfjkasdf"
             }
         }
     }
